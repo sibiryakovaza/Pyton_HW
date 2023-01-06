e216ff5fc6fb40e1f560cb3987e8bd9eac28f380
sibiryakovaza <114570433+sibiryakovaza@users.noreply.github.com>
2023-01-06 17:59:23 +0300

    Update task1.py

--- les_4/task1.py
+++ les_4/task1.py
@@ -19,3 +19,18 @@ def calcLevel(d):
 
     return numer
   
+def calcPi(numlevel):
+
+    # присвоения числа π (инициализация)
+    num = Decimal( math.pi)
+    print(f'Число π (Без точности в кол-ве знаков): {num}')
+
+    numOut = num.quantize( Decimal(numlevel) )
+    print(f'Число π (C точностью до {len(numlevel)-2} знаков): {numOut}')
+
+
+
+d = input("Укажите точность (кол-во знаков после запятой ) : ")
+numlevel = calcLevel(d)
+
+calcPi(numlevel)
