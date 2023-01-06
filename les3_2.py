15eefda98dcae6798832cba849fe3146d93d1549
sibiryakovaza <114570433+sibiryakovaza@users.noreply.github.com>
2023-01-06 17:36:57 +0300

    Update les3_2.py

--- les3_2.py
+++ les3_2.py
@@ -1,2 +1,25 @@
 #  Напишите программу, которая найдёт произведение пар чисел списка.
 # Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+from random import sample
+
+
+def list_rand_nums(count):
+    if count < 0:
+        print("negative value of the numbers")
+        return[]
+      
+    list_nums = sample(range(1, count * 2), count)
+    return list_nums
+  
+  
+def prod_pairs(list_nums: list):
+    res_list = []
+    len_list = len(list_nums)
+    
+    for k in range(len_list // 2):
+        res_list.append(list_nums[k] * list_nums[len_list - k - 1])
+        
+    if len_list % 2:
+        res_list.append(list_nums[len_list // 2])
+    return res_list
