7ab7370691811eb8833f795801fb5d810876c966
sibiryakovaza <114570433+sibiryakovaza@users.noreply.github.com>
2023-01-06 17:57:12 +0300

    Update task1.py

--- les_4/task1.py
+++ les_4/task1.py
@@ -1,2 +1,21 @@
 # Вычислить число c заданной точностью d
 
+import math #импорт модуля МАТЕМАТИКА
+from decimal import Decimal #импорт модуля ....
+
+# №1. конвертация ввода в input() в вид "1.000...."
+def calcLevel(d):
+
+    #  отрисовка кол-ва нулей, которое введено в input()
+    arr1 = [0 for i in range( int(d) )]
+    #print(arr1, type(arr1[0])) # ---> [0, 0, 0, 0] <class 'int'>
+
+    # перерисовка в вид "1.00000"
+    numer = ''
+    for i in arr1:
+        numer +=str(i)
+    numer = '1.'+numer
+    #print(numer, type (numer))
+
+    return numer
+  
